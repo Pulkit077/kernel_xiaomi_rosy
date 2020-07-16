796a9f3750325f6416d94d2b8476afff61fe9391
Wei Wang <wvw@google.com>
2020-07-15 22:57:41 -0700

    sched: fair: placement optimization for heavy load
    Previously we have used pure CFS wakeup in overutilized case. This is a
    tweaked version to activate the path only for important tasks.
    
    Bug: 161190988
    Bug: 160883639
    Test: boot and systrace
    Signed-off-by: Wei Wang <wvw@google.com>
    Change-Id: I2a27f241b3ba32a04cf6f88deb483d6636440dcf
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7230,6 +7230,12 @@ retry:
 				}
 
 				/*
+				 * Skip searching for active CPU for tasks have
+				 * high priority & prefer_high_cap.
+				 */
+				if (boosted && p->prio <= DEFAULT_PRIO)
+					continue;
+				/*
 				 * Case A.2: Target ACTIVE CPU
 				 * Favor CPUs with max spare capacity.
 				 */
@@ -7445,8 +7451,10 @@ retry:
 		: best_idle_cpu;
 
 	if (target_cpu == -1 && most_spare_cap_cpu != -1 &&
-		/* ensure we use active cpu for active migration */
-		!(p->state == TASK_RUNNING && !idle_cpu(most_spare_cap_cpu)))
+	    /* ensure we use active cpu for active migration */
+	    !(p->state == TASK_RUNNING && !idle_cpu(most_spare_cap_cpu)) &&
+		/* do not pick an overutilized most_spare_cap_cpu */
+		!cpu_overutilized(most_spare_cap_cpu))
 		target_cpu = most_spare_cap_cpu;
 
 	if (cpu_isolated(prev_cpu)) {
