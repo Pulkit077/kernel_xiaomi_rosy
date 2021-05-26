4b20821d1f0e7d1e5f2f379038e25fd015ad9a2b
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-05-26 08:59:41 +0530

    drivers: smbcharger: remove rerun_hvdcp_det_if_necessary function
     we rerun APSD everytime so useless
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- drivers/power/supply/qcom/qpnp-smbcharger.c
+++ drivers/power/supply/qcom/qpnp-smbcharger.c
@@ -8428,6 +8428,7 @@ static int smbchg_check_chg_version(struct smbchg_chip *chip)
 	return 0;
 }
 
+/*
 static void rerun_hvdcp_det_if_necessary(struct smbchg_chip *chip)
 {
 	enum power_supply_type usb_supply_type;
@@ -8467,7 +8468,7 @@ static void rerun_hvdcp_det_if_necessary(struct smbchg_chip *chip)
 			pr_err("Couldn't vote for 0 for suspend wa, going ahead rc=%d\n",
 					rc);
 
-		/* Schedule work for HVDCP detection */
+		// Schedule work for HVDCP detection
 		if (!chip->hvdcp_not_supported) {
 			cancel_delayed_work_sync(&chip->hvdcp_det_work);
 			smbchg_stay_awake(chip, PM_DETECT_HVDCP);
@@ -8475,7 +8476,7 @@ static void rerun_hvdcp_det_if_necessary(struct smbchg_chip *chip)
 					msecs_to_jiffies(HVDCP_NOTIFY_MS));
 		}
 	}
-}
+}*/
 
 static void usb_type_check_work_fn(struct smbchg_chip *chip)
 {
@@ -8811,7 +8812,7 @@ static int smbchg_probe(struct platform_device *pdev)
 		goto unregister_led_class;
 	}
 
-	rerun_hvdcp_det_if_necessary(chip);
+	//rerun_hvdcp_det_if_necessary(chip);
 
 	update_usb_status(chip, is_usb_present(chip), false);
 	dump_regs(chip);
