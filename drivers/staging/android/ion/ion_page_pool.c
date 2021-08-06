f682047fd5b3f4f592c665900f162b85d1f9baab
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-08-06 13:03:20 +0530

    Revert "ION: Improve cache accounting"
    Revert "ION:move to the node page api for ion pool"
    
    This reverts commit fbcb0424ec32c43b2fbcc1cc377b6beeaa203efe.
    
    Revert "ION: Improve cache accounting"
    
    This reverts commit a4d857b548c6059febf4abe74fe81799668ce8bd.
    
     - already been implemented in commit ("mm: add ION heap-pool allocation stat into vmstat")
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- drivers/staging/android/ion/ion_page_pool.c
+++ drivers/staging/android/ion/ion_page_pool.c
@@ -28,8 +28,6 @@
 #include <linux/swap.h>
 #include <linux/vmalloc.h>
 #include <linux/compaction.h>
-#include <linux/vmstat.h>
-#include <linux/mmzone.h>
 #include "ion_priv.h"
 
 #define ION_PAGE_CACHE	1
@@ -74,8 +72,6 @@ static void ion_page_pool_free_pages(struct ion_page_pool *pool,
 
 static int ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 {
-	int page_count = 1 << pool->order;
-
 	mutex_lock(&pool->mutex);
 	page->private = ION_PAGE_CACHE;
 	if (PageHighMem(page)) {
@@ -90,9 +86,6 @@ static int ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 		__SetPageMovable(page, pool->inode->i_mapping);
 	mod_node_page_state(page_pgdat(page), NR_INDIRECTLY_RECLAIMABLE_BYTES,
 			    (1 << (PAGE_SHIFT + pool->order)));
-	mod_node_page_state(page_pgdat(page), NR_FILE_PAGES, page_count);
-	mod_node_page_state(page_pgdat(page), NR_INACTIVE_FILE, page_count);
-
 	mutex_unlock(&pool->mutex);
 	return 0;
 }
@@ -100,7 +93,6 @@ static int ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 static struct page *ion_page_pool_remove(struct ion_page_pool *pool, bool high)
 {
 	struct page *page;
-	int page_count = 1 << pool->order;
 
 	if (high) {
 		BUG_ON(!pool->high_count);
@@ -117,10 +109,6 @@ static struct page *ion_page_pool_remove(struct ion_page_pool *pool, bool high)
 	list_del_init(&page->lru);
 	mod_node_page_state(page_pgdat(page), NR_INDIRECTLY_RECLAIMABLE_BYTES,
 			    -(1 << (PAGE_SHIFT + pool->order)));
-
-	mod_node_page_state(page_pgdat(page), NR_INACTIVE_FILE, -page_count);
-	mod_node_page_state(page_pgdat(page), NR_FILE_PAGES, -page_count);
-
 	return page;
 }
 
